a4bf7031c5d556110b32892f4fa500d660a207eb
Jon Haataja <jon.haataja@gmail.com>
2025-04-24 15:33:32 +0300

    Added folder and api file

---
+++ src/lib/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { setupCache } from 'axios-cache-interceptor';
+import { TypeData } from './types';
+
+const instance = axios.create(); 
+const axiosInstance = setupCache(instance);
+
+export async function fetchPokemonData(name: string) {
+  const response = await axiosInstance.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase().trim()}`);
+  return response.data;
+}
+
+export async function fetchTypeData(url: string): Promise<TypeData> {
+  const response = await axios.get<TypeData>(url);
+  return response.data;
+}
