f60e6cda4b37e6578d47c81c8955d72165b41940
Jon Haataja <jon.haataja@gmail.com>
2025-04-24 21:29:03 +0300

    Installed @vitejs/plugin-react and added error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "@fontsource-variable/pixelify-sans";
+import ErrorBoundary from './components/ErrorBoundary';
 
 createRoot(document.getElementById("root")!).render(
+  <ErrorBoundary>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </ErrorBoundary>,
 );
