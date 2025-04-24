cc0e39e559a74b98ebcfdda8d54217c68ad6a757
Jon Haataja <jon.haataja@gmail.com>
2025-04-24 15:34:07 +0300

    Added logic file

---
+++ src/lib/logic.ts
@@ -0,0 +1,14 @@
+import { fetchTypeData } from './api';
+
+export async function getPokemonWeaknesses(types: string[]) {
+  const weaknesses: Set<string> = new Set();
+
+  for (const type of types) {
+    const data = await fetchTypeData(`https://pokeapi.co/api/v2/type/${type}`);
+    data.damage_relations.double_damage_from.forEach((typeObj) => {
+      weaknesses.add(typeObj.name);
+    });
+  }
+
+  return Array.from(weaknesses);
+}
