3b894f289c7ae740ee810c2c069a0b3f9c0e5bad
Jon Haataja <jon.haataja@gmail.com>
2025-04-24 20:02:35 +0300

    Installed vitest and jest. Wrote a few unit tests for front page.

---
+++ src/__tests__/Home.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import App from '../App.tsx'
+
+describe('Home', () => {
+
+    it('should have a Search text', () => {
+        render(<App/>) // ARRANGE
+    
+        const searchBtn = screen.getByText('Search') // ACT
+    
+        expect(searchBtn).toBeInTheDocument() // ASSERT
+    })
+
+    it('should have an h1 heading"', () => {
+        render(<App/>)
+    
+        const h1 = screen.getByRole('heading', {
+            level: 1,
+            name: 'Pokémon Weakness Finder'
+        })
+    
+        expect(h1).toBeInTheDocument()
+    })
+
+})
