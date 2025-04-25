0f2254d6faa362f924476d6eca93a74c2b37e35a
Jon Haataja <jon.haataja@gmail.com>
2025-04-25 15:52:16 +0300

    Ran prettier

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,10 +8,7 @@ export default defineConfig({
     test: {
       globals: true,
       environment: "jsdom",
-      setupFiles: [
-        "./vite.setup.ts",
-        "./src/setupTests.ts"
-      ],
+      setupFiles: ["./vite.setup.ts", "./src/setupTests.ts"],
     },
     exclude: [...configDefaults.exclude, "**/e2e/**"], // Example: Exclude e2e tests
     coverage: {
