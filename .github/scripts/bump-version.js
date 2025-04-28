8662e8a06981138062b39185479fc492f692518a
Jon Haataja <jon.haataja@gmail.com>
2025-04-28 16:04:58 +0300

    Modified ci-cd.yaml and added bump-version.js to bump up the extension version number automatically.

---
+++ .github/scripts/bump-version.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+// Finding manifest.json from the root
+const manifestPath = path.join(__dirname, '../../manifest.json');
+
+// Reading and updating the version number
+const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+const [major, minor, patch] = manifest.version.split('.').map(Number);
+const newVersion = `${major}.${minor}.${patch + 1}`;
+manifest.version = newVersion;
+
+// Writing the new version
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+
+console.log(`Version bumped to ${newVersion}`);
